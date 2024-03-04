285686ee1faf31fc394a66e96adad9bdab5e32e1
gabriel-04-10 <156429999+gabriel-04-10@users.noreply.github.com>
2024-03-04 19:21:39 +0200

    hand hitbox ( almost finished)

--- HMW-3/Assets/Scripts/Hand.cs
+++ HMW-3/Assets/Scripts/Hand.cs
@@ -1,53 +1,90 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Data.Common;
 using UnityEngine;
 
 [RequireComponent(typeof(Animator))]
 public class Hand : MonoBehaviour
 {
-    public float speed;
-    Animator animator;
-    private float triggerTarget;
-    private float gripTarget;
-    private float gripCurrent;
-    private float triggerCurrent;
-    private string animatorGripParam = "Grip";
-    private string animatorTriggerParam = "Trigger";
+    [SerializeField] private float animationSpeed;
+    private Animator _animator;
+    private SkinnedMeshRenderer _mesh;
+    private float _gripTarget;
+    private float _triggerTarget;
+    private float _gripCurrent;
+    private float _triggerCurrent;
+    private const string AnimatorGripParam = "Grip";
+    private const string AnimatorTriggerParam = "Trigger";
+    private static readonly int Grip = Animator.StringToHash(name: AnimatorGripParam);
+    private static readonly int Triger = Animator.StringToHash(name: AnimatorTriggerParam);
+
+    // physics movement
+    [SerializeField] private GameObject followObject;
+    [SerializeField] private float followSpeed = 30f;
+    [SerializeField] private float rotateSpeed = 100f;
+    [SerializeField] private Vector3 positionOffset;
+    [SerializeField] private Vector3 rotationOffset;
+    private Transform _followTarget;
+    private Rigidbody _body;
+
+    private void Start()
+    {
+        // physics movement
+        _followTarget = followObject.transform;
+        _body = GetComponent<Rigidbody>();
+        _body.collisionDetectionMode = CollisionDetectionMode.Continuous;
+        _body.interpolation = RigidbodyInterpolation.Interpolate;
+        _body.mass = 20f;
+
+        // teleport hands
+        _body.position = _followTarget.position;
+        _body.rotation = _followTarget.rotation;
+    }
+
+    private void Update()
+    {
+        AnimateHand();
+        PhysicsMove();
+    }
+
+    private void PhysicsMove()
+    {
+        // position 
+        var positionWithOffset = _followTarget.position;
+        var distance = Vector3.Distance(positionWithOffset, transform.position);
+        _body.velocity = (_followTarget.position - transform.position).normalized * (followSpeed * distance);
+
+        // rotation
+        var rotationWithOffset = _followTarget.rotation * Quaternion.Euler(rotationOffset);
+        var q = rotationWithOffset * Quaternion.Inverse(_body.rotation);
+        q.ToAngleAxis(out float angle, out Vector3 axis);
+        _body.angularVelocity = axis * (angle * Mathf.Deg2Rad * rotateSpeed);
+    }
 
     internal void SetGrip(float v)
     {
-        gripTarget = v;
+        _gripTarget = v;
     }
 
     internal void SetTrigger(float v)
     {
-        triggerTarget = v;
+        _triggerTarget = v;
     }
 
     
-    void Start()
-    {
-        animator = GetComponent<Animator>();
-    }
-
     
-    void Update()
+    private void AnimateHand()
     {
-        AnimateHand();
-    }
-    
-    void AnimateHand()
-    {
-        if (gripCurrent != gripTarget)
+        if (Math.Abs(_gripCurrent-_gripTarget)>0)
         {
-            gripCurrent = Mathf.MoveTowards(gripCurrent, gripTarget,Time.deltaTime*speed);
-            animator.SetFloat(animatorGripParam,gripCurrent);
+            _gripCurrent = Mathf.MoveTowards(_gripCurrent, _gripTarget,maxDelta: Time.deltaTime*animationSpeed);
+            _animator.SetFloat(AnimatorGripParam,_gripCurrent);
         }
-        if (triggerCurrent != triggerTarget)
+        if (Math.Abs(_triggerCurrent-_triggerTarget)>0)
         {
-            triggerCurrent = Mathf.MoveTowards(triggerCurrent, triggerTarget,Time.deltaTime*speed);
-            animator.SetFloat(animatorTriggerParam,triggerCurrent);
+            _triggerCurrent = Mathf.MoveTowards(_triggerCurrent, _triggerTarget,Time.deltaTime*animationSpeed);
+            _animator.SetFloat(AnimatorTriggerParam,_triggerCurrent);
         }
     }
 }
