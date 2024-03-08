fd211d1e09093c9c59b393910148a3d8301d228b
gabriel-04-10 <156429999+gabriel-04-10@users.noreply.github.com>
2024-03-08 19:11:25 +0200

    salle terminé
    left to do :
    buttons

---
+++ HMW-3/Assets/XRGrabInteractableBothHands.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class XRGrabInteractableBothHands : XRGrabInteractable
+{
+    public Transform leftAttachTransform;
+    public Transform rightAttachTransform;
+    protected override void OnSelectEntered(SelectEnterEventArgs args)
+    {
+        if(args.interactorObject.transform.CompareTag("left hand"))
+        {
+            attachTransform = leftAttachTransform;
+        }
+        else if(args.interactorObject.transform.CompareTag("right hand"))
+        {
+            attachTransform = rightAttachTransform;
+        }
+
+        base.OnSelectEntered(args);
+    }
+}
